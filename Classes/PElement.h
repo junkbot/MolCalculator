68ad23f08b5d54493580153bb2abdc6eb48d7202
Christian Knappskog <ch_knapp@mac.com>
2011-05-28 16:04:41 +0200

    Added alot more data to each element

--- Classes/PElement.h
+++ Classes/PElement.h
@@ -4,16 +4,34 @@
 @interface PElement : NSObject {
 	NSString* name;
 	NSString* abbreviation;
-	float weight;
+	double weight;
 	int number;
 }
 
 @property (nonatomic, retain) NSString *name;
 @property (nonatomic, retain) NSString *abbreviation;
-@property (nonatomic, assign) float weight;
+@property (nonatomic, assign) double weight;
 @property (nonatomic, assign) int number;
 
-+ (PElement*) elementWithName:(NSString*)name number: (int)number abbreviation:(NSString*)abbreviation	andWeight:(float)weight;
++ (PElement*) elementWithName:(NSString*)name number: (int)number abbreviation:(NSString*)abbreviation	andWeight:(double)weight;
+
++ (PElement*) elementWithNumber:        (int)number   
+                    abbreviation:       (NSString*)abbreviation   
+                    groupNumber:        (int)groupNumber   
+                    groupName:          (NSString*)groupName
+                    period:             (int)period 
+                    name:               (NSString*)name      
+                    roundedMass:        (float)roundedMass      
+                    mass:               (double)weight    
+                    meltingPoint:       (double)meltingPoint 
+                    boilingPoint:       (double)boilingPoint 
+                    density:            (double)density
+                    electroNegativity:  (double)electroNegativity 
+                    firstIonisation:    (int)firstIonisation 
+                    electronAffinity:   (int)electronAffinity  
+                    commonOxidation:    (NSString*)commonOxidation 		    
+                    predictedConfiguration: (NSString*)predictedConfiguration
+                    observedConfiguration:  (NSString*)observedConfiguration;     
 
 - (NSComparisonResult)compare:(id)otherObject;
 
--- Classes/PElement.m
+++ Classes/PElement.m
@@ -18,8 +18,34 @@
     return [self.name compare:[otherObject name]];
 }
 
-+ (PElement*) elementWithName:(NSString*)name number: (int)number abbreviation:(NSString*)abbreviation	andWeight:(float)weight
++ (PElement*) elementWithName:(NSString*)name number: (int)number abbreviation:(NSString*)abbreviation	andWeight:(double)weight
 {
+	PElement *element = [[PElement alloc] init];
+	element.name = name;
+	element.weight = weight;
+	element.number = number;
+	element.abbreviation = abbreviation;
+    [element autorelease];
+	return element;
+}
+
++ (PElement*) elementWithNumber:        (int)number   
+                   abbreviation:       (NSString*)abbreviation   
+                    groupNumber:        (int)groupNumber   
+                      groupName:          (NSString*)groupName
+                         period:             (int)period 
+                           name:               (NSString*)name      
+                    roundedMass:        (float)roundedMass      
+                           mass:               (double)weight    
+                   meltingPoint:       (double)meltingPoint 
+                   boilingPoint:       (double)boilingPoint 
+                        density:            (double)density
+              electroNegativity:  (double)electroNegativity 
+                firstIonisation:    (int)firstIonisation 
+               electronAffinity:   (int)electronAffinity  
+                commonOxidation:    (NSString*)commonOxidation 		    
+         predictedConfiguration: (NSString*)predictedConfiguration
+          observedConfiguration:  (NSString*)observedConfiguration{
 	PElement *element = [[PElement alloc] init];
 	element.name = name;
 	element.weight = weight;
